67e782f523c7b57160c45e47ab3999ee4bad4a3b
Peter Arnold <peter@deepsixdesign.com>
2020-06-16 09:26:15 -0400

    lookin skook

---
+++ entroprise-admin/src/components/sentences.js
@@ -0,0 +1,83 @@
+// in src/Comments.js
+import * as React from "react"
+// tslint:disable-next-line:no-var-requires
+import {
+  Datagrid,
+  List,
+  Show,
+  Create,
+  Edit,
+  DateInput,
+  DateField,
+  SimpleShowLayout,
+  SimpleForm,
+  TextField,
+  TextInput,
+  ShowButton,
+  EditButton,
+  DeleteButton,
+  RichTextField,
+  SelectInput,
+} from "react-admin"
+import RichTextInput from "ra-input-rich-text"
+
+export const SentenceList = props => (
+  <List {...props}>
+    <Datagrid>
+      <TextField source="title" />
+      <RichTextField source="body" />
+      <ShowButton label="" />
+      <EditButton label="" />
+      <DeleteButton label="" redirect={false} />
+    </Datagrid>
+  </List>
+)
+
+export const SentenceShow = props => (
+  <Show {...props}>
+    <SimpleShowLayout>
+      <TextField source="id" />
+      <TextField source="title" />
+      <RichTextField source="body" />
+      <DateField source="createdate" options={{ disabled: true }} />
+      <DateField source="lastupdate" options={{ disabled: true }} />
+    </SimpleShowLayout>
+  </Show>
+)
+
+export const SentenceCreate = props => (
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput source="title" />
+      <RichTextInput source="body" />
+      <SelectInput
+        source="rating"
+        choices={[
+          { id: 1, name: "Good" },
+          { id: 2, name: "Okay" },
+          { id: 3, name: "Bad" },
+        ]}
+      />
+    </SimpleForm>
+  </Create>
+)
+
+export const SentenceEdit = props => (
+  <Edit {...props}>
+    <SimpleForm>
+      <TextInput source="id" options={{ disabled: true }} />
+      <DateInput source="createdate" options={{ disabled: true }} />
+      <DateInput source="lastupdate" options={{ disabled: true }} />
+      <TextInput source="title" />
+      <RichTextInput source="body" />
+      <SelectInput
+        source="rating"
+        choices={[
+          { id: 1, name: "Good" },
+          { id: 2, name: "Okay" },
+          { id: 3, name: "Bad" },
+        ]}
+      />
+    </SimpleForm>
+  </Edit>
+)
--- entroprise-admin/src/pages/index.js
+++ entroprise-admin/src/pages/index.js
@@ -4,15 +4,60 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import { Admin, Resource, ListGuesser } from "react-admin"
-import jsonServerProvider from "ra-data-json-server"
-const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com")
+import { Admin, Resource } from "react-admin"
+import CustomLoginPage from "../components/CustomLoginPage"
+
+import {
+  SentenceList,
+  SentenceShow,
+  SentenceCreate,
+  SentenceEdit,
+} from "../components/sentences"
+
+import {
+  FirebaseAuthProvider,
+  FirebaseDataProvider,
+} from "react-admin-firebase"
+
+// import { firebaseConfig as config } from "../configs/FIREBASE_CONFIG"
+
+import CommentOutlinedIcon from "@material-ui/icons/CommentOutlined"
+
+const config = {
+  apiKey: "AIzaSyBWp-Akak-fGxdpjaUvInSOCpMck_16SAA",
+  authDomain: "entroprise-production.firebaseapp.com",
+  databaseURL: "https://entroprise-production.firebaseio.com",
+  projectId: "entroprise-production",
+  storageBucket: "entroprise-production.appspot.com",
+  messagingSenderId: "22266004444",
+  appId: "1:22266004444:web:3219338e78c23d1e370519",
+  measurementId: "G-R5YR48J1QY",
+}
+
+const options = {
+  logging: true,
+  rootRef: "root/items",
+}
+
+const dataProvider = FirebaseDataProvider(config, options)
+const authProvider = FirebaseAuthProvider(config, options)
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Admin dataProvider={dataProvider}>
-      <Resource name="users" list={ListGuesser} />
+    <Admin
+      loginPage={CustomLoginPage}
+      dataProvider={dataProvider}
+      authProvider={authProvider}
+    >
+      <Resource
+        name="sentences"
+        icon={CommentOutlinedIcon}
+        list={SentenceList}
+        show={SentenceShow}
+        create={SentenceCreate}
+        edit={SentenceEdit}
+      />
     </Admin>
   </Layout>
 )
